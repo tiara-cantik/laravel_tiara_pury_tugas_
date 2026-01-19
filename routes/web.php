27afb5e9a9e8a56cb8dc8ccc19406c04eeb9d76b
tiara-cantik <tiarafajriatus9@gmail.com>
2026-01-19 09:02:36 +0700

    Update Tugas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\SekolahController;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/profil/{id}', [SekolahController::class, 'show']);
+Route::get('/posts', [PostController::class, 'index'])->name('posts.index');
+Route::get('/posts/{id}', [PostController::class, 'show'])->name('posts.show');
+Route::get('/about-me', [PostController::class, 'about'])->name('posts.about');
